ff1b100cdbafbd11426a96d7eabc6a6d457d82c3
pares-seux <paresseux.992@gmail.com>
2022-02-28 11:16:47 +0400

    Added additional task

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,6 +3,15 @@ const menu = () => {
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
   const menuItems = menu.querySelectorAll("ul>li>a");
+  const scrollBtn = document.querySelector("main>a");
+
+  const scroll = (element) => {
+    const time = 500;
+      let href = element.getAttribute('href');
+      console.log(href, document.getElementById(href.slice(1)));
+      let target = document.getElementById(href.slice(1));
+      target.scrollIntoView({block: "center", behavior: "smooth"});    
+  };
 
   const handleMenu = () => {
     menu.classList.toggle("active-menu");
@@ -11,8 +20,16 @@ const menu = () => {
   closeBtn.addEventListener("click", handleMenu);
   menuBtn.addEventListener("click", handleMenu);
   menuItems.forEach((menuItem) =>
-    menuItem.addEventListener("click", handleMenu)
+    menuItem.addEventListener("click", (event) => {
+      event.preventDefault();
+      scroll(menuItem);
+      handleMenu();
+    })
   );
+  scrollBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    scroll(scrollBtn);
+  });
 };
 
 export default menu;
