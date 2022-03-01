841304b7b55edadaab9bac8f72eec1cbf9a85d6a
pares-seux <paresseux.992@gmail.com>
2022-03-01 22:13:56 +0400

    finished with regexp

--- src/modules/regexp.js
+++ src/modules/regexp.js
@@ -1,7 +1,7 @@
-
-
 const regexp = () => {
-  const calcInputs = document.querySelectorAll(".calc-block > input[type=text]");
+  const calcInputs = document.querySelectorAll(
+    ".calc-block > input[type=text]"
+  );
   const textInputs = document.querySelectorAll("form input[type=text]");
   const emailInputs = document.querySelectorAll("form input[type=email]");
   const telInputs = document.querySelectorAll("form input[type=tel]");
@@ -9,47 +9,57 @@ const regexp = () => {
   errorSpan.innerText = "*Значение введено неверно";
   errorSpan.style.cssText = " margin-bottom: 10px;";
 
-
-  textInputs.forEach( (item) => {
-    item.addEventListener('blur', () => {
-      if(/[^а-яА-Я\s-]+/g.test(item.value)) {
+  textInputs.forEach((item) => {
+    item.addEventListener("blur", () => {
+      if (/[^а-яА-Я\s-]+/g.test(item.value)) {
         item.parentElement.append(errorSpan);
       } else {
         errorSpan.remove();
-        item.value = item.value.replace(/(^[^а-яА-Я]+)|([^а-яА-Я]+)$/g, '').replace(/\s\s/g, ' ').replace(/--/g, '-');
+        item.value = item.value
+          .replace(/(^[^а-яА-Я]+)|([^а-яА-Я]+)$/g, "")
+          .replace(/\s\s/g, " ")
+          .replace(/--/g, "-");
       }
     });
   });
 
-  emailInputs.forEach( (item) => {
-    item.addEventListener('blur', () => {
-      if(/[^a-zA-z0-9@-_\.!~\*\']+/g.test(item.value)) {
+  emailInputs.forEach((item) => {
+    item.addEventListener("blur", () => {
+      if (/[^a-zA-z0-9@-_\.!~\*\']+/g.test(item.value)) {
         item.parentElement.append(errorSpan);
       } else {
         errorSpan.remove();
-        item.value = item.value.replace(/(^[^a-zA-z0-9]+)|([^a-zA-z0-9]+)$/g, '').replace(/@@/g, '@').replace(/\.\./g, '\.');
+        item.value = item.value
+          .replace(/(^[^a-zA-z0-9]+)|([^a-zA-z0-9]+)$/g, "")
+          .replace(/@@/g, "@")
+          .replace(/\.\./g, ".");
       }
     });
   });
 
-  telInputs.forEach( (item) => {
-      item.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/^\d-()]+/g,"");
-      });
-      item.addEventListener('blur', () => {
-        if (item.value.length < 10) {
-          item.parentElement.append(errorSpan);
-        }else {
-        errorSpan.remove();};
-      });
-
-  });
-
-  calcInputs.forEach( (item) => {
-    item.addEventListener('input', (e) => {
-      e.target.value = e.target.value.replace(/[\D]+/,"");
+  telInputs.forEach((item) => {
+    item.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/^\d-()]+/g, "");
+    });
+    item.addEventListener("blur", () => {
+      item.value = item.value.replace(/[()-]/g, "");
+      if (item.value.length < 10) {
+        item.parentElement.append(errorSpan);
+      } else {
+        errorSpan.remove();
+        item.value = `(${item.value.slice(0, 3)})${item.value.slice(
+          3,
+          6
+        )}-${item.value.slice(6, 8)}-${item.value.slice(8, 10)}`;
+      }
     });
   });
-}
+
+  calcInputs.forEach((item) => {
+    item.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[\D]+/, "");
+    });
+  });
+};
 
 export default regexp;
