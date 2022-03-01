63324a7e8d371c52feb337283f011bb19f455090
pares-seux <paresseux.992@gmail.com>
2022-03-01 17:29:23 +0400

    added regexp for number

--- src/modules/regexp.js
+++ src/modules/regexp.js
@@ -9,6 +9,7 @@ const regexp = () => {
   errorSpan.innerText = "*Значение введено неверно";
   errorSpan.style.cssText = " margin-bottom: 10px;";
 
+
   textInputs.forEach( (item) => {
     item.addEventListener('blur', () => {
       if(/[^а-яА-Я\s-]+/g.test(item.value)) {
@@ -17,9 +18,32 @@ const regexp = () => {
         errorSpan.remove();
         item.value = item.value.replace(/(^[^а-яА-Я]+)|([^а-яА-Я]+)$/g, '').replace(/\s\s/g, ' ').replace(/--/g, '-');
       }
-    });ц
+    });
   });
 
+  emailInputs.forEach( (item) => {
+    item.addEventListener('blur', () => {
+      if(/[^a-zA-z0-9@-_\.!~\*\']+/g.test(item.value)) {
+        item.parentElement.append(errorSpan);
+      } else {
+        errorSpan.remove();
+        item.value = item.value.replace(/(^[^a-zA-z0-9]+)|([^a-zA-z0-9]+)$/g, '').replace(/@@/g, '@').replace(/\.\./g, '\.');
+      }
+    });
+  });
+
+  telInputs.forEach( (item) => {
+      item.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/^\d-()]+/g,"");
+      });
+      item.addEventListener('blur', () => {
+        if (item.value.length < 10) {
+          item.parentElement.append(errorSpan);
+        }else {
+        errorSpan.remove();};
+      });
+
+  });
 
   calcInputs.forEach( (item) => {
     item.addEventListener('input', (e) => {
