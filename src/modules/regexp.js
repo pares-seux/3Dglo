9709c4c5b1f7e1e5f56d5168ccba08614cc4c6c2
pares-seux <paresseux.992@gmail.com>
2022-03-03 08:31:55 +0400

    Fixed #1

--- src/modules/regexp.js
+++ src/modules/regexp.js
@@ -3,55 +3,64 @@ const regexp = () => {
     ".calc-block > input[type=text]"
   );
   const textInputs = document.querySelectorAll("form input[type=text]");
+  const areaInput = document.querySelector(".mess");
   const emailInputs = document.querySelectorAll("form input[type=email]");
   const telInputs = document.querySelectorAll("form input[type=tel]");
-  const errorSpan = document.createElement("p");
-  errorSpan.innerText = "*Значение введено неверно";
-  errorSpan.style.cssText = " margin-bottom: 10px;";
+// const errorSpan = document.createElement("p");
+// errorSpan.innerText = "*Значение введено неверно";
+// errorSpan.style.cssText = " margin-bottom: 10px;";
 
   textInputs.forEach((item) => {
+    item.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^а-яА-Я\s-]+/g, "");
+    });
     item.addEventListener("blur", () => {
-      if (/[^а-яА-Я\s-]+/g.test(item.value)) {
-        item.parentElement.append(errorSpan);
-      } else {
-        errorSpan.remove();
         item.value = item.value
           .replace(/(^[^а-яА-Я]+)|([^а-яА-Я]+)$/g, "")
-          .replace(/\s\s/g, " ")
-          .replace(/--/g, "-");
-      }
+          .replace(/([-()@_.!~*'])(?=[-()@_.!~*']*\1)/g, '');
     });
   });
 
-  emailInputs.forEach((item) => {
+  areaInput.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^а-яА-Я\s-]+/g, "");
+    });
     item.addEventListener("blur", () => {
-      if (/[^a-zA-z0-9@-_\.!~\*\']+/g.test(item.value)) {
-        item.parentElement.append(errorSpan);
-      } else {
-        errorSpan.remove();
         item.value = item.value
-          .replace(/(^[^a-zA-z0-9]+)|([^a-zA-z0-9]+)$/g, "")
-          .replace(/@@/g, "@")
-          .replace(/\.\./g, ".");
-      }
+          .replace(/(^[^а-яА-Я]+)|([^а-яА-Я]+)$/g, "")
+          .replace(/([-()@_.!~*'])(?=[-()@_.!~*']*\1)/g, '');
+    });
+
+
+
+  emailInputs.forEach((item) => {
+    item.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^a-zA-z0-9@-_\.!~\*\']+/g, "");
+    });
+    item.addEventListener("blur", () => {
+      item.value = item.value
+          .replace(/([-()@_.!~*'])(?=[-()@_.!~*']*\1)/g, '');
     });
   });
 
   telInputs.forEach((item) => {
     item.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(/^\d-()]+/g, "");
+      e.target.value = e.target.value.replace(/[^\d-()]+/g, "");
     });
     item.addEventListener("blur", () => {
       item.value = item.value.replace(/[()-]/g, "");
-      if (item.value.length < 10) {
-        item.parentElement.append(errorSpan);
-      } else {
-        errorSpan.remove();
+      if (item.value.length === 10) {
         item.value = `(${item.value.slice(0, 3)})${item.value.slice(
           3,
           6
         )}-${item.value.slice(6, 8)}-${item.value.slice(8, 10)}`;
+      } else if (item.value.length === 11 && (item.value[0] === '7' || item.value[0] === '8' )) {
+        {
+        item.value = `+7(${item.value.slice(1, 4)})${item.value.slice(
+          4,
+          7
+        )}-${item.value.slice(7, 9)}-${item.value.slice(9, 11)}`;
       }
+    }
     });
   });
 
