5a2c010c9e6d3850326c8b809dfb36532e374ee6
pares-seux <paresseux.992@gmail.com>
2022-03-07 16:21:35 +0400

    lesson 24

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal";
 import regexp from "./modules/regexp";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import calc from "./modules/calc";
 
 timer("25 february 2022");
 menu();
@@ -11,3 +12,4 @@ modal();
 regexp();
 tabs();
 slider("portfolio-content", "portfolio-item");
+calc();
