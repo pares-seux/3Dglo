ad90b3eec61c20d2608368bf773f8a5d05231b51
pares-seux <paresseux.992@gmail.com>
2022-03-15 10:29:53 +0400

    removed animation again

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -5,23 +5,6 @@ const calc = (price = 100) => {
   const calcCount = document.querySelector(".calc-count");
   const calcDay = document.querySelector(".calc-day");
   const total = document.getElementById("total");
-  let totalAnimate,
-    counter = 0,
-    animateFrame;
-
-  const animate = (totalValue) => {
-    const step = (totalValue - +total.textContent) / 100;
-    return function animate() {
-      animateFrame = requestAnimationFrame(totalAnimate);
-
-      if (counter < totalValue || counter > totalValue) {
-        counter += step;
-        total.textContent = counter;
-      } else {
-        cancelAnimationFrame(animateFrame);
-      }
-    };
-  };
 
   const countCalc = () => {
     const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
@@ -47,24 +30,19 @@ const calc = (price = 100) => {
       totalValue = 0;
     }
 
-    totalAnimate = animate(totalValue);
-    totalAnimate();
+    total.textContent = totalValue;
   };
 
-  calcBlock.addEventListener(
-    "blur",
-    (e) => {
-      if (
-        e.target === calcType ||
-        e.target === calcSquare ||
-        e.target === calcCount ||
-        e.target === calcDay
-      ) {
-        countCalc();
-      }
-    },
-    true
-  );
+  calcBlock.addEventListener("input", (e) => {
+    if (
+      e.target === calcType ||
+      e.target === calcSquare ||
+      e.target === calcCount ||
+      e.target === calcDay
+    ) {
+      countCalc();
+    }
+  });
 };
 
 export default calc;
